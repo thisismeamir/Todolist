48a4cd816fc7166d5e18880f40dd51265b3ca00f
Amir <thisismeamir@outlook.com>
2021-12-08 11:03:34 +0330

    Initial connections and main.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import datetime as dt
+def Init():
+     with open('./database.txt', 'r') as file:
+          datalive = []
+          for line in file:
+               line = line.split("-")
+               # ['[x]','2.3.13.','asdasdas']
+               data = {
+                    'condition': line[0],
+                    'date': line[1],
+                    'description': line[2]
+               }  
+               datalive.append(data)   
+          print(datalive)          
+               
 
-while True:
-     question = ""
-     userInput = input(question)
-     if userInput == 'exit':
-          break
+
+def main():
+     Init()
+     while True:
+          userInput = input("What to do now? ")
+
+          if userInput == 'new':
+               inpu
+          elif userInput == 'delete':
+               pass
+          
+          elif userInput == 'exit':
+               
+               break
+          
+
+if __name__ == '__main__':
+     main()
